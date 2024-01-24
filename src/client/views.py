6d27a8e266f085ab6a1792ede40bd2cb1923b780
Yannick KAGOU <112559433+yannkagou@users.noreply.github.com>
2024-01-25 00:30:49 +0100

    Add the Client's views, urls and serializers

--- src/client/views.py
+++ src/client/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Client
+from .serializers import ClientSerializer
+
+from team.models import Team
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+    
+    def get_queryset(self):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        return self.queryset.filter(team=team)
+    
+    def perform_create(self, serializer):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        serializer.save(team=team, created_by=self.request.user)
+
